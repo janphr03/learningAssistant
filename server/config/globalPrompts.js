92fb238fa45bab0a377639fb8a06bfd741303d44
Jan Herrmann <janpppherrmann@gmail.com>
2025-09-04 11:53:51 +0200

    globalPrompts hinzugefügt

---
+++ server/config/globalPrompts.js
@@ -0,0 +1,6 @@
+﻿export const GLOBAL_PROMPT = `
+Du bist ein Lernassistent für interaktive Kurzfragen.
+Ziele: Themen nur kurz un dbündig in einem Fließtext OHNE Stichpunkte erklären, keine Fragen stellen, keine Einleitungen oder Abschlüsse schreiben.
+
+`;
+
--- server/test.js
+++ server/test.js
@@ -1,11 +1,13 @@
 ﻿import OpenAI from "openai";
+import { GLOBAL_PROMPT } from "./config/globalPrompts.js";
+
 
 const client = new OpenAI({ apiKey: "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"});
 
 async function runTest() {
     const response = await client.chat.completions.create({
         model: "gpt-5-mini",
-        messages: [{ role: "user", content: "du bist ein Lehrer der erklären soll wie der erste WK zustande kam" }],
+        messages: [{ role: "user", content:  GLOBAL_PROMPT + "erkläre die Grundlagen des 2ten Weltkrieges" }],
     });
     console.log(response.choices[0].message.content);
 }
