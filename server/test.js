cea8a44c4fa11a7dea5cb195b3028bd19fb86320
Jan Herrmann <janpppherrmann@gmail.com>
2025-09-04 11:31:14 +0200

    init commit develop

--- server/test.js
+++ server/test.js
@@ -5,9 +5,10 @@ const client = new OpenAI({ apiKey: "sk-proj-7DCRiiEB-c8ew4fFy0Oa6D7Fdl41WR5erq7
 async function runTest() {
     const response = await client.chat.completions.create({
         model: "gpt-5-mini",
-        messages: [{ role: "user", content: "erkläre mir was zur deutschen Historie" }],
+        messages: [{ role: "user", content: "du bist ein Lehrer der erklären soll wie der erste WK zustande kam" }],
     });
     console.log(response.choices[0].message.content);
 }
 
 runTest();
+
